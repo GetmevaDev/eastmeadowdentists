46961e304352cea2304bec3189ca8b07b497c08a
Arsen <arsen@getmeva.com>
2022-11-01 09:25:34 +0500

    fix gallery

--- src/components/BeforeAfter/beforeAfter.js
+++ src/components/BeforeAfter/beforeAfter.js
@@ -4,21 +4,12 @@ import { graphql, useStaticQuery } from "gatsby"
 const TeethComponent = () => {
   const teeth = useStaticQuery(graphql`
     {
-      allStrapiGallerybBeforeAfter {
-        edges {
-          node {
-            right {
-              img {
-                url
-              }
-              text
-            }
-            left {
-              img {
-                url
-              }
-              text
-            }
+      strapiGallerybBeforeAfter {
+        GalleryList {
+        text
+          img {
+            alternativeText
+            url
           }
         }
       }
@@ -29,36 +20,47 @@ const TeethComponent = () => {
 
   return (
     <div className="container">
-      {teeth.allStrapiGallerybBeforeAfter.edges.map((item, index) => (
-        <div className="gallery__main" key={index}>
-          <div className="gallery__left">
-            {item.node.left.map((left, index) => (
-              <div className="gallery__left-inner" key={index}>
-                <img
-                  className="gallery__right--img"
-                  src={left.img[0].url}
-                  alt=""
-                  key={index}
-                />
-                <span className="gallery__text">{left.text}</span>
-              </div>
-            ))}
-          </div>
-          <div className="gallery__right">
-            {item.node.right.map((right, index) => (
-              <div className="gallery__right-inner" key={index}>
-                <img
-                  className="gallery__left--img"
-                  src={right.img[0].url}
-                  alt=""
-                />
+      <div className={`gallery__main`}>
+      {
+        teeth.strapiGallerybBeforeAfter.GalleryList.map((item, index) => (
 
-                <span className="gallery__text">{right.text}</span>
-              </div>
-            ))}
+            <div key={index} className={`picture`}>
+              <img src={item.img[0].url} alt={item.img[0].alternativeText}/>
+                <p className={`gallery__text`}>{item.text}</p>
           </div>
-        </div>
-      ))}
+        ))
+      }
+      </div>
+      {/*{teeth.allStrapiGallerybBeforeAfter.edges.map((item, index) => (*/}
+      {/*  <div className="gallery__main" key={index}>*/}
+      {/*    <div className="gallery__left">*/}
+      {/*      {item.node.left.map((left, index) => (*/}
+      {/*        <div className="gallery__left-inner" key={index}>*/}
+      {/*          <img*/}
+      {/*            className="gallery__right--img"*/}
+      {/*            src={left.img[0].url}*/}
+      {/*            alt=""*/}
+      {/*            key={index}*/}
+      {/*          />*/}
+      {/*          <span className="gallery__text">{left.text}</span>*/}
+      {/*        </div>*/}
+      {/*      ))}*/}
+      {/*    </div>*/}
+      {/*    <div className="gallery__right">*/}
+      {/*      {item.node.right.map((right, index) => (*/}
+      {/*        <div className="gallery__right-inner" key={index}>*/}
+      {/*          <img*/}
+      {/*            className="gallery__left--img"*/}
+      {/*            src={right.img[0].url}*/}
+      {/*            alt=""*/}
+      {/*          />*/}
+
+      {/*          <span className="gallery__text">{right.text}</span>*/}
+      {/*        </div>*/}
+      {/*      ))}*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*))}*/}
     </div>
   )
 }
