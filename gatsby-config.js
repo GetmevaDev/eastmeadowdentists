6b2c1d19fce9b95a44b9617bb8eeaf7760fcc3ee
Arsen <arsen@getmeva.com>
2022-11-01 09:59:16 +0500

    fix testimonial

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: "https://cms-all-smiles-dental-spa.herokuapp.com",
-        // apiURL: "http://localhost:1337",
+        // apiURL: "https://cms-all-smiles-dental-spa.herokuapp.com",
+        apiURL: "http://localhost:1337",
         // ? "https://cms-all-smiles-dental-spa.herokuapp.com"
         // : "http://localhost:1337",
         queryLimit: 1000, // Default to 100
@@ -73,7 +73,7 @@ module.exports = {
         icon: `src/images/android-chrome-512x512.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-preact`,
+    // `gatsby-plugin-preact`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -1,12 +1,14 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 import SectionHeader from "../components/RepeatComponents/sectionHeader"
 import SectionComments from "../components/Home-Page-Components/sectionComment"
-import Layout from "../components/layout"
 import SectionAppointment from "../components/RepeatComponents/sectionAppointment"
 import SectionMap from "../components/RepeatComponents/sectionMap"
-import SEO from "../components/seo"
+
 import SectionReviewForm from "../components/Testimonials/sectionReviewForm"
-import { graphql, useStaticQuery } from "gatsby"
+
 
 function TestimonialsPage() {
   const data = useStaticQuery(graphql`
