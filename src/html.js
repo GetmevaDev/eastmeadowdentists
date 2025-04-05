059920497265320842631d3b9cef948d7760a660
GetmevaDev <75464863+GetmevaDev@users.noreply.github.com>
2025-04-05 18:01:58 +0300

    Revert html.js

--- src/html.js
+++ src/html.js
@@ -5,24 +5,15 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
     <head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116300824-2"></script>
       <script defer src="https://book.getweave.com/text-connect/69dca75e-383c-4dd4-ac52-918c8a5a9cb3/widget.js"></script>
-    
-      
-      <script>{`(function (w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({
-          'gtm.start':
-              new Date().getTime(), event: 'gtm.js'
-        });
-        var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-      })(window, document, 'script', 'dataLayer', 'GTM-WM9CV49N');`}</script>
-    
-    
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-116300824-2');`}}/>
       <meta charSet="utf-8" />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta
@@ -34,19 +25,14 @@ export default function HTML(props) {
       {props.headComponents}
     </head>
     <body {...props.bodyAttributes}>
-   
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WM9CV49N"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    
-
-      {props.preBodyComponents}
-      <div
-        key={`body`}
-        id="___gatsby"
-        dangerouslySetInnerHTML={{ __html: props.body }}
-      />
-      {props.postBodyComponents}
-      <script src="//code.tidio.co/8lasjfcaqnmx8gexjih5djbwxoeuc8un.js" defer></script>
+    {props.preBodyComponents}
+    <div
+      key={`body`}
+      id="___gatsby"
+      dangerouslySetInnerHTML={{ __html: props.body }}
+    />
+    {props.postBodyComponents}
+    <script src="//code.tidio.co/8lasjfcaqnmx8gexjih5djbwxoeuc8un.js" defer></script>
     </body>
     </html>
   )
