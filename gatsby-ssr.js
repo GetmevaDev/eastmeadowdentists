6fb1320b9c6133e0d8c2ab57fda52234a3915bbc
GetmevaDev <75464863+GetmevaDev@users.noreply.github.com>
2025-04-05 18:28:09 +0300

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,31 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+import React from "react"
 
-// You can delete this file if you're not using it
+export function onRenderBody({ setHeadComponents, setHtmlAttributes, setBodyAttributes, setPreBodyComponents, setPostBodyComponents }) {
+  // GTM Script
+  setHeadComponents([
+    <script
+      key="gtm-script"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WM9CV49N');
+        `,
+      }}
+    />,
+  ])
+
+  // GTM noscript fallback
+  setPreBodyComponents([
+    <noscript key="gtm-noscript">
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-WM9CV49N"
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+      />
+    </noscript>,
+  ])
+}
