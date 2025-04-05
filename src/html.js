f128cddd4911d943242dd8bc1f4fed90e59bcb40
GetmevaDev <75464863+GetmevaDev@users.noreply.github.com>
2025-04-05 18:26:03 +0300

    Update html.js

--- src/html.js
+++ src/html.js
@@ -5,16 +5,7 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
     <head>
-    
       <script defer src="https://book.getweave.com/text-connect/69dca75e-383c-4dd4-ac52-918c8a5a9cb3/widget.js"></script>
-     
-      <script>{
-        "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-WM9CV49N');"
-        }</script>
 
       <meta charSet="utf-8" />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
@@ -27,9 +18,6 @@ export default function HTML(props) {
       {props.headComponents}
     </head>
     <body {...props.bodyAttributes}>
-
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WM9CV49N"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
     {props.preBodyComponents}
     <div
